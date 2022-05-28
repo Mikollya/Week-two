52b7a99aaaaf5579d81c84d12e7335b4dde6f8d4
Mikollya <mykola.kurets.knm.2020@lpnu.ua>
2022-05-28 19:02:17 +0300

    Add code for project and update README file.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Week-two
+Task:
+    create landing page, one screen, using flex, grid, media screen, position, pre-procesors scss(sass) etc.
---
+++ js/main.js
@@ -0,0 +1,11 @@
+const OPEN_CLASSNAME = "active";
+
+const navLinks = document.getElementById("nav-links");
+const headerBurger = document.getElementById("header__burger");
+const body = document.getElementById("body");
+
+function toggleMenu() {
+    navLinks.classList.toggle(OPEN_CLASSNAME);
+    headerBurger.classList.toggle(OPEN_CLASSNAME);
+    body.classList.toggle("locke");
+}
